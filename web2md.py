53ec444f88056e7f247510b16a282aec320afbe8
wolverinn <36463336+wolverinn@users.noreply.github.com>
2019-12-20 10:15:28 +0800

    html2txt

---
+++ web2md.py
@@ -0,0 +1,28 @@
+import html2text
+import requests
+import re
+import os
+# sudo apt-get install python3-tk
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+}
+
+def get_flow(page_url):
+    h = html2text.HTML2Text()
+    h.ignore_links = False
+    h.ignore_images = False
+    h.ignore_emphasis = False
+    h.ignore_tables = False
+    filename = "{}.txt".format(page_url.split('/')[-1])
+    f = open(filename,'w',encoding='utf-8')
+    r = requests.get(page_url,headers=headers)
+    if r.status_code == 200:
+        converted_content = h.handle(r.text)
+        f.write(converted_content)
+        f.write('\n')
+    f.close()
+
+page_url = input("输入链接：")
+get_flow(page_url)
+print("required page saved successfully")
